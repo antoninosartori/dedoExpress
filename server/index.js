5e3e8e07f87428608ebb32f29b592ec22e0a4c70
antoninosartori <sartoriantonino@gmail.com>
2023-07-28 13:26:36 -0300

    express path para las rutas estaticas

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 require('dotenv').config()
 const PORT = process.env.PORT || 3001
 const express = require('express')
+const path = require('path')
 const app = express()
 require('./connection/mongo.js')
 const cors = require('cors')
 
-app.use(cors())
-app.use(express.json({limit: '25mb'}));
-app.use(express.urlencoded({limit: '25mb'}));
+// para que encuentre las rutas de navegacion 
+const buildPath = path.join(__dirname, 'client', 'dist');
+app.use(express.static(buildPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(buildPath, 'index.html'));
+});
 
+app.use(cors())
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb' }));
 
 const handleError = require('./middlewares/handleError.js')
 const notFound = require('./middlewares/notFound.js')
