379a7f9441e05c67591a78e43a8072579702de5e
antoninosartori <sartoriantonino@gmail.com>
2023-08-01 20:52:52 -0300

    actualziado updateTravel and userDetails

--- client/src/pages/UpdateTravelPage.jsx
+++ client/src/pages/UpdateTravelPage.jsx
@@ -4,117 +4,148 @@ import useUpdateTravel from '../hooks/useUpdateTravel'
 import useGetOneTravel from '../hooks/useGetOneTravel'
 import { useForm } from 'react-hook-form'
 import { validateDate } from '../helpers/validateDate'
-import IconDiv from '../components/IconDiv'
 import FloatinNotification from '../components/FloatinNotification'
+import Header from '../components/Header'
 import Button from '../components/Button'
-import chevronDownIcon from '../assets/chevron-down.svg'
-import editIcon from '../assets/pencil.svg'
-import toIcon from '../assets/to.svg'
-import capacityIcon from '../assets/people-fill.svg'
-import priceIcon from '../assets/currency-dollar.svg'
-import dateTimeIcon from '../assets/time_arrive.svg'
+import TextWithTitle from '../components/TextWithTitle'
+import RowItemWithIcon from '../components/RowItemWithIcon'
+import petIcon from '../assets/pet_supplies.svg'
+import luggageIcon from '../assets/luggage.svg'
+import musicIcon from '../assets/music_note.svg'
+import fastfoodIcon from '../assets/fastfood.svg'
+import voiceIcon from '../assets/voice_selection.svg'
 
 export default function UpdateTravelPage() {
    const { register, handleSubmit, formState: { errors }, setValue } = useForm()
    const { travel } = useGetOneTravel()
    const { handleSumbitUpdateTravel } = useUpdateTravel()
 
-   // verificar si el usuario es el mismoq que creo ya que pueden entrar desde la url para modificar un viaje cualquiera
-   /* useEffect(() => {
-      const userIdFromTravel = travel.user ? travel.user[0]._id : null
-      if(user.userId !== userIdFromTravel){
-         return navigate('/')
-      }
-   }, [travel._id]) */
-
    useEffect(() => {
       setValue('from', travel.from)
       setValue('to', travel.to)
       setValue('capacity', travel.capacity)
       setValue('price', travel.price)
+      setValue('pet', travel.features?.pet)
+      setValue('food', travel.features?.food)
+      setValue('luggage', travel.features?.luggage)
+      setValue('talk', travel.features?.talk)
+      setValue('music', travel.features?.music)
    }, [travel._id])
 
+   console.log({ travel })
+
    return (
-      <main className='container updateTravelPage'>
-         <div className='updateTravel-header'>
-            <img className='icon' src={editIcon} alt="editar viaje" />
-            <h2 className='title'>Modificar viaje</h2>
-         </div>
+      <>
+         <Header />
+         <main className='container updateTravelPage'>
+            <header>
+               <h2 className='subtitle'>Modificar viaje</h2>
+               <article>
+                  <TextWithTitle noTitle>
+                     <p>Podés cambiar cualquier aspecto de tu viaje.</p>
+                  </TextWithTitle>
+               </article>
+            </header>
 
-         <form onSubmit={handleSubmit(handleSumbitUpdateTravel)}>
+            <form onSubmit={handleSubmit(handleSumbitUpdateTravel)}>
 
-            <div className="formGroup">
-               <label htmlFor="from">Lugar de salida:</label>
-               <input
-                  {...register('from', {
-                     required: 'Completa el lugar de salida con una ciudad',
-                     maxLength: { value: 30, message: 'La ciudad de salida debe ser menor a 30 caracteres' },
-                     minLength: { value: 3, message: 'El nombre de la ciudad de salida debe ser mayor a 3 caracteres' }
-                  })}
-                  type="text" autoComplete='off' />
-            </div>
-            {errors.from?.message && < FloatinNotification message={errors.from.message} />}
+               <div className="form-separator">
+                  <div className="formGroup">
+                     <label htmlFor="from">¿Desde dónde salís?</label>
+                     <input
+                        {...register('from', {
+                           required: 'Completa el lugar de salida con una ciudad',
+                           maxLength: { value: 30, message: 'La ciudad de salida debe ser menor a 30 caracteres' },
+                           minLength: { value: 3, message: 'El nombre de la ciudad de salida debe ser mayor a 3 caracteres' }
+                        })}
+                        type="text" autoComplete='off' placeholder='Lugar de salida' />
+                  </div>
+                  {errors.from?.message && < FloatinNotification message={errors.from.message} />}
 
-            < IconDiv icon1={chevronDownIcon} icon2={toIcon} />
+                  <div className="formGroup">
+                     <label htmlFor="to">¿Hacia dónde vas?</label>
+                     <input
+                        {...register('to', {
+                           required: 'Completa la ciudad de destino',
+                           minLength: { value: 3, message: 'La ciudad de destino debe ser mayor a 3 caracteres' },
+                           maxLength: { value: 30, message: 'La ciudad de destino debe ser menor a 30 caracteres' }
+                        })}
+                        type="text" autoComplete='off' placeholder='Lugar de destino' />
+                  </div>
+                  {errors.to?.message && < FloatinNotification message={errors.to.message} />}
 
-            <div className="formGroup">
-               <label htmlFor="to">Destino:</label>
-               <input
-                  {...register('to', {
-                     required: 'Completa la ciudad de destino',
-                     minLength: { value: 3, message: 'La ciudad de destino debe ser mayor a 3 caracteres' },
-                     maxLength: { value: 30, message: 'La ciudad de destino debe ser menor a 30 caracteres' }
-                  })}
-                  type="text" autoComplete='off' />
-            </div>
-            {errors.to?.message && < FloatinNotification message={errors.to.message} />}
+                  <div className="formGroup">
+                     <label htmlFor="capacity">¿Cuántas personas quieres llevar?</label>
+                     <input
+                        {...register('capacity', {
+                           required: 'Completa el campo de lugares disponibles',
+                           min: { value: 1, message: 'El minimo de lugares disponibles es 1' },
+                           max: { value: 9, message: 'El maximo de lugares disponibles es de 9' }
+                        })}
+                        type="number" autoComplete='off' min={1} placeholder='Lugares disponibles' />
+                  </div>
+                  {errors.capacity?.message && < FloatinNotification message={errors.capacity.message} />}
 
-            < IconDiv icon1={chevronDownIcon} icon2={capacityIcon} />
+                  <div className="formGroup">
+                     <label htmlFor="price">¿Precio por persona?</label>
+                     <input
+                        {...register('price', {
+                           required: 'Completa el campo del precio por persona',
+                           max: { value: 50000, message: 'El precio maximo es de 50.000' },
+                           min: 0
+                        })}
+                        type="number" autoComplete='off' min={0} placeholder='Precio por persona' />
+                  </div>
+                  {errors.price?.message && < FloatinNotification message={errors.price.message} />}
 
-            <div className="formGroup">
-               <label htmlFor="capacity">Lugares disponibles:</label>
-               <input
-                  {...register('capacity', {
-                     required: 'Completa el campo de lugares disponibles',
-                     min: { value: 1, message: 'El minimo de lugares disponibles es 1' },
-                     max: { value: 9, message: 'El maximo de lugares disponibles es de 9' }
-                  })}
-                  type="number" autoComplete='off' min={1} />
-            </div>
-            {errors.capacity?.message && < FloatinNotification message={errors.capacity.message} />}
+                  <div className="formGroup">
+                     <label htmlFor="dateTime">¿Fecha y hora de salida?</label>
+                     <input
+                        {...register('dateTime', {
+                           required: 'Por favor, confirma la fecha y hora de salida',
+                           validate: value => validateDate(value) || 'Ingresa una fecha futura'
+                        })}
+                        type="datetime-local" placeholder='Fecha y hora de salida' />
+                  </div>
+               </div>
+               {errors.dateTime?.message && < FloatinNotification message={errors.dateTime.message} />}
 
-            < IconDiv icon1={chevronDownIcon} icon2={priceIcon} />
+               <div className="form-separator">
+                  <TextWithTitle title='Adicionales (opcionales)' >
+                     <p>Marca aquellas características que podrían ser interesantes para las personas que quieran viajar con vos</p>
+                  </TextWithTitle>
 
-            <div className="formGroup">
-               <label htmlFor="price">Precio por persona:</label>
-               <input
-                  {...register('price', {
-                     required: 'Completa el campo del precio por persona',
-                     max: { value: 50000, message: 'El precio maximo es de 50.000' },
-                     min: 0
-                  })}
-                  type="number" autoComplete='off' min={0} />
-            </div>
-            {errors.price?.message && < FloatinNotification message={errors.price.message} />}
+                  <div className="ceckbox-inputContainer">
+                     <input {...register('pet')}
+                        type="checkbox" />
+                     <RowItemWithIcon icon={petIcon} text='Mascotas permitidas' />
+                  </div>
+                  <div className="ceckbox-inputContainer">
+                     <input {...register('luggage')}
+                        type="checkbox" />
+                     <RowItemWithIcon icon={luggageIcon} text='Baúl para equipaje' />
+                  </div>
+                  <div className="ceckbox-inputContainer">
+                     <input {...register('music')}
+                        type="checkbox" />
+                     <RowItemWithIcon icon={musicIcon} text='Buena música' />
+                  </div>
+                  <div className="ceckbox-inputContainer">
+                     <input {...register('food')}
+                        type="checkbox" />
+                     <RowItemWithIcon icon={fastfoodIcon} text='Permitido comer' />
+                  </div>
+                  <div className="ceckbox-inputContainer">
+                     <input {...register('talk')}
+                        type="checkbox" />
+                     <RowItemWithIcon icon={voiceIcon} text='Charlatán' />
+                  </div>
+               </div>
 
-            < IconDiv icon1={chevronDownIcon} icon2={dateTimeIcon} />
-
-            <div className="formGroup">
-               <label htmlFor="dateTime">Fecha y hora de salida:</label>
-               <input
-                  {...register('dateTime', {
-                     required: 'Por favor, confirma la fecha y hora de salida',
-                     validate: value => validateDate(value) || 'Ingresa una fecha futura'
-                  })}
-                  type="datetime-local" />
-            </div>
-            {errors.dateTime?.message && < FloatinNotification message={errors.dateTime.message} />}
-
-            <div className="formGroup">
                <Button>Modificar viaje</Button>
-            </div>
 
-         </form>
-      </main>
+            </form>
+         </main>
+      </>
    )
 }
--- client/src/pages/UserDetailsPage.jsx
+++ client/src/pages/UserDetailsPage.jsx
@@ -5,6 +5,7 @@ import useGetOneUser from '../hooks/useGetOneUser'
 import { useForm } from 'react-hook-form'
 import LoadingSpinner from '../components/LoadingSpinner'
 import FloatinNotification from '../components/FloatinNotification'
+import Header from '../components/Header'
 import Button from '../components/Button'
 import InputAvatar from '../components/InputAvatar'
 import { REGEX_EMAIL } from '../helpers/consts'
@@ -27,81 +28,84 @@ export default function UserDetailsPage() {
    }, [account])
 
    return (
-      <main className='container userDetailsPage'>
-         <h2 className='title'>Detalles de la cuenta</h2>
+      <>
+         < Header />
+         <main className='container userDetailsPage'>
+            <h2 className='subtitle'>Detalles de la cuenta</h2>
 
-         {isLoading && < LoadingSpinner text='cargando detalles...' />}
-         {floatingNotification.message &&
-            < FloatinNotification
-               message={floatingNotification.message}
-               status={floatingNotification.status}
-               duration={floatingNotification.duration}
-            />
-         }
+            {isLoading && < LoadingSpinner text='cargando detalles...' />}
+            {floatingNotification.message &&
+               < FloatinNotification
+                  message={floatingNotification.message}
+                  status={floatingNotification.status}
+                  duration={floatingNotification.duration}
+               />
+            }
 
-         {
-            !isLoading &&
-            <form onSubmit={handleSubmit(handleSubmitUpdateAccount)}>
+            {
+               !isLoading &&
+               <form onSubmit={handleSubmit(handleSubmitUpdateAccount)}>
 
-               <InputAvatar register={register} handleAvatarChange={handleAvatarChange} avatarPreview={avatarPreview} />
-               {errors.avatar?.message && < FloatinNotification message={errors.avatar.message} />}
+                  <InputAvatar register={register} handleAvatarChange={handleAvatarChange} avatarPreview={avatarPreview} />
+                  {errors.avatar?.message && < FloatinNotification message={errors.avatar.message} />}
 
-               <div className="formGroup">
-                  <label htmlFor="name">Nombre:</label>
-                  <input
-                     {...register('name', {
-                        required: 'El campo del nombre esta vacio',
-                        minLength: { value: 3, message: 'El campo del nombre debe ser mayor a 3 caracteres' },
-                        maxLength: { value: 30, message: 'El campo del nombre debe ser menor a 30 caracteres' }
-                     })}
-                     type="text"
-                  />
-               </div>
-               {errors.name?.message && < FloatinNotification message={errors.name.message} />}
+                  <div className="formGroup">
+                     <label htmlFor="name">Nombre:</label>
+                     <input
+                        {...register('name', {
+                           required: 'El campo del nombre esta vacio',
+                           minLength: { value: 3, message: 'El campo del nombre debe ser mayor a 3 caracteres' },
+                           maxLength: { value: 30, message: 'El campo del nombre debe ser menor a 30 caracteres' }
+                        })}
+                        type="text"
+                     />
+                  </div>
+                  {errors.name?.message && < FloatinNotification message={errors.name.message} />}
 
-               <div className="formGroup">
-                  <label htmlFor="username">Username:</label>
-                  <input
-                     {...register('username', {
-                        required: 'El campo del username esta vacio',
-                        minLength: { value: 3, message: 'El campo del username debe ser mayor a 3 caracteres' },
-                        maxLength: { value: 30, message: 'El campo del username debe ser menor a 30 caracteres' }
-                     })}
-                     type="text" />
-               </div>
-               {errors.username?.message && < FloatinNotification message={errors.username.message} />}
+                  <div className="formGroup">
+                     <label htmlFor="username">Username:</label>
+                     <input
+                        {...register('username', {
+                           required: 'El campo del username esta vacio',
+                           minLength: { value: 3, message: 'El campo del username debe ser mayor a 3 caracteres' },
+                           maxLength: { value: 30, message: 'El campo del username debe ser menor a 30 caracteres' }
+                        })}
+                        type="text" />
+                  </div>
+                  {errors.username?.message && < FloatinNotification message={errors.username.message} />}
 
-               <div className="formGroup">
-                  <label htmlFor="email">Email:</label>
-                  <input
-                     {...register('email', {
-                        required: 'El campo del email esta vacio',
-                        pattern: {
-                           value: REGEX_EMAIL,
-                           message: 'Debes introducir un email valido'
-                        }
-                     })}
-                     type="email" />
-               </div>
-               {errors.email?.message && < FloatinNotification message={errors.email.message} />}
+                  <div className="formGroup">
+                     <label htmlFor="email">Email:</label>
+                     <input
+                        {...register('email', {
+                           required: 'El campo del email esta vacio',
+                           pattern: {
+                              value: REGEX_EMAIL,
+                              message: 'Debes introducir un email valido'
+                           }
+                        })}
+                        type="email" />
+                  </div>
+                  {errors.email?.message && < FloatinNotification message={errors.email.message} />}
 
-               <div className="formGroup">
-                  <label htmlFor="cellphone">Celular:</label>
-                  <input
-                     {...register('cellphone', {
-                        required: 'Ingresa un numero de telefono',
-                        maxLength: { value: 10, message: 'El numero de telefono no puede tener mas de 10 digitos' },
-                        minLength: { value: 10, message: 'El numero de telefono no puede tener menos de 10 digitos' }
-                     })}
-                     type="number" />
-               </div>
-               {errors.cellphone?.message && < FloatinNotification message={errors.cellphone.message} />}
+                  <div className="formGroup">
+                     <label htmlFor="cellphone">Celular:</label>
+                     <input
+                        {...register('cellphone', {
+                           required: 'Ingresa un numero de telefono',
+                           maxLength: { value: 10, message: 'El numero de telefono no puede tener mas de 10 digitos' },
+                           minLength: { value: 10, message: 'El numero de telefono no puede tener menos de 10 digitos' }
+                        })}
+                        type="number" />
+                  </div>
+                  {errors.cellphone?.message && < FloatinNotification message={errors.cellphone.message} />}
 
-               <Button type='submit'>Guardar cambios</Button>
+                  <Button primary type='submit'>Guardar cambios</Button>
 
-            </form>
-         }
+               </form>
+            }
 
-      </main>
+         </main>
+      </>
    )
 }
