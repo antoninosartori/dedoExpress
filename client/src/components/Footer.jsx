e4c4b4c741314d247ed6a34b8cba56611de61a55
antoninosartori <sartoriantonino@gmail.com>
2023-08-27 19:08:27 -0300

    footer con icons

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,29 @@
 import './Footer.css'
+import InstagramIcon from '../assets/InstagramIcon'
+import LinkedInIcon from '../assets/LinkedIn'
+import SiteIcon from '../assets/SiteIcon'
 export default function Footer() {
    return (
       <footer className='footer-component'>
          <div className='footer-flex'>
-            <p>
-               <div className='DedoExpress-footer'>
+            <div className='footer-textContainer'>
+               <p>
                   <span className='bold'>Dedo</span>
-                  <span>Express</span>
-               </div>
-               es un producto de
-               <a className='hover bold' target='_blank' rel='noreferrer' href="https://kumpel.com.ar/">Kumpel</a>
-            </p>
+                  Express es un producto de <a href="https://kumpel.com.ar/" target='_blank' rel='noreferrer' className='hover bold'>Kumpel</a>
+               </p>
+            </div>
+            <div className='footer-iconsContainer'>
+               <a href="https://kumpel.com.ar/" target='_blank' rel='noreferrer' className='hover'>
+                  < SiteIcon />
+               </a>
+               <a href="https://www.linkedin.com/company/kumpel/" target='_blank' rel='noreferrer' className='hover'>
+                  < LinkedInIcon />
+               </a>
+               <a href="https://www.instagram.com/kumpelweb/?igshid=MzRlODBiNWFlZA%3D%3D" target='_blank' rel='noreferrer' className='hover'>
+                  < InstagramIcon />
+               </a>
+
+            </div>
          </div>
       </footer>
    )
