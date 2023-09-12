230cafe33dbf7b6a8db76043887e26ea31682a1a
antoninosartori <sartoriantonino@gmail.com>
2023-09-12 15:47:55 -0300

    fix text error HelpPage

--- client/src/pages/HelpPage.jsx
+++ client/src/pages/HelpPage.jsx
@@ -13,7 +13,7 @@ export default function HelpPage() {
 
             <TextWithTitle title='¡Ayudanos a mejorar!'>
                <p>Estamos abiertos para recibir sugerencias y comentarios.</p>
-               <p>Si tenés algún error o querés preguntarnos alo podés hacerlo via Whatsapp</p>
+               <p>Si tenés algún error o querés preguntarnos algo podés hacerlo via Whatsapp</p>
             </TextWithTitle>
             <Button type='button' primary>
                <a href={`https://api.whatsapp.com/send/?phone=3446544456&text=Hola, quiero comunicarme para...`} target='_blank' rel='noreferrer'>
