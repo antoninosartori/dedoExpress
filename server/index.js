783f717ff8e78eedd5d82d5250db65d47b8494b5
antoninosartori <sartoriantonino@gmail.com>
2023-09-14 18:26:48 -0300

    add cors con el /#/

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ Sentry.init({
    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!,
 });
 
-app.use(cors())
+app.use(cors({origin: 'https://dedoexpress.vercel.app/#/'}))
 
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb' }));
