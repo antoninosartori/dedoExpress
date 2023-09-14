fae4f9d2b46c79c76bb3e8c9aa2cf666b8257a7a
antoninosartori <sartoriantonino@gmail.com>
2023-09-14 18:00:33 -0300

    agregando config de vercel de cors

--- server/index.js
+++ server/index.js
@@ -11,24 +11,38 @@ const cors = require('cors')
 Sentry.init({
    dsn: "https://630bc1dc36897bd619acd05d5765efa1@o4505749248475136.ingest.sentry.io/4505749250703360",
    integrations: [
-     // enable HTTP calls tracing
-     new Sentry.Integrations.Http({
-       tracing: true
-     }),
-     // enable Express.js middleware tracing
-     new Sentry.Integrations.Express({
-       app
-     }),
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({
+         tracing: true
+      }),
+      // enable Express.js middleware tracing
+      new Sentry.Integrations.Express({
+         app
+      }),
    ],
    // Performance Monitoring
    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!,
- });
+});
 
 // app.use(cors())
-app.use(cors({
-   origin: 'https://dedoexpress.vercel.app',
- }));
+const allowCors = fn => async (req, res) => {
+   res.setHeader('Access-Control-Allow-Credentials', true)
+   res.setHeader('Access-Control-Allow-Origin', '*')
+   // otra configuración común
+   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+   res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+   )
+   if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+   }
+   return await fn(req, res)
+}
 
+app.use(allowCors);
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb' }));
 
