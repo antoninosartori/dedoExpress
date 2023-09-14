d320169d1467f5f3ab439ba65290d234d1e0f1b4
antoninosartori <sartoriantonino@gmail.com>
2023-09-14 18:28:44 -0300

    add (8)

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ Sentry.init({
    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!,
 });
 
-app.use(cors({origin: 'https://dedoexpress.vercel.app/#/'}))
+app.use(cors(`*`))
 
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb' }));
